85855e681c277a9e2e524f92ee3e71e80db18df3
Natty2999 <nataliarakovicka@gmail.com>
2024-10-01 13:31:23 +0200

    simple python script to replace wool with shulkers in minecraft command

---
+++ replace_wool_with_shulker.py
@@ -0,0 +1,45 @@
+command_name = 'rainbow_road'
+command = 'summon falling_block ~ ~1 ~ {BlockState:{Name:activator_rail},Time:1,Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Passengers:[{id:"command_block_minecart",Command:"kill @e[type=command_block_minecart,distance=..3]"}],Command:"fill ~ ~-1 ~-1 ~ ~-1 ~-2 magenta_wool"}],Command:"fill ~ ~0 ~-15 ~ ~-1 ~-16 orange_wool"}],Command:"fill ~ ~0 ~-13 ~ ~-1 ~-14 orange_wool"}],Command:"fill ~ ~0 ~-11 ~ ~-1 ~-12 orange_wool"}],Command:"fill ~ ~0 ~-9 ~ ~-1 ~-10 orange_wool"}],Command:"fill ~ ~0 ~-7 ~ ~-1 ~-8 orange_wool"}],Command:"fill ~ ~0 ~-3 ~ ~-1 ~-3 red_wool"}],Command:"fill ~ ~1 ~-15 ~ ~1 ~-16 red_wool"}],Command:"fill ~ ~1 ~-4 ~ ~-1 ~-6 red_wool"}],Command:"fill ~ ~1 ~-1 ~ ~0 ~-2 magenta_wool"}],Command:"fill ~ ~2 ~-8 ~ ~1 ~-8 red_wool"}],Command:"fill ~ ~2 ~-6 ~ ~1 ~-7 red_wool"}],Command:"setblock ~ ~2 ~-5 magenta_wool"}],Command:"fill ~ ~2 ~-2 ~ ~1 ~-3 magenta_wool"}],Command:"setblock ~ ~2 ~-1 purple_wool"}],Command:"fill ~ ~3 ~-15 ~ ~2 ~-16 red_wool"}],Command:"fill ~ ~3 ~-12 ~ ~1 ~-14 red_wool"}],Command:"fill ~ ~3 ~-9 ~ ~1 ~-11 red_wool"}],Command:"setblock ~ ~3 ~-8 magenta_wool"}],Command:"fill ~ ~3 ~-3 ~ ~2 ~-4 magenta_wool"}],Command:"setblock ~ ~3 ~-2 purple_wool"}],Command:"fill ~ ~4 ~-6 ~ ~3 ~-7 magenta_wool"}],Command:"fill ~ ~4 ~-4 ~ ~3 ~-5 magenta_wool"}],Command:"setblock ~ ~4 ~-3 purple_wool"}],Command:"fill ~ ~5 ~-16 ~ ~4 ~-16 magenta_wool"}],Command:"fill ~ ~5 ~-14 ~ ~4 ~-15 magenta_wool"}],Command:"fill ~ ~5 ~-12 ~ ~4 ~-13 magenta_wool"}],Command:"fill ~ ~5 ~-10 ~ ~4 ~-11 magenta_wool"}],Command:"fill ~ ~5 ~-8 ~ ~4 ~-9 magenta_wool"}],Command:"setblock ~ ~5 ~-7 purple_wool"}],Command:"setblock ~ ~5 ~-6 blue_wool"}],Command:"fill ~ ~5 ~-1 ~ ~3 ~-1 blue_wool"}],Command:"fill ~ ~6 ~-2 ~ ~4 ~-2 blue_wool"}],Command:"fill ~ ~7 ~-16 ~ ~6 ~-16 blue_wool"}],Command:"fill ~ ~7 ~-14 ~ ~6 ~-15 blue_wool"}],Command:"fill ~ ~7 ~-12 ~ ~6 ~-13 blue_wool"}],Command:"fill ~ ~7 ~-10 ~ ~6 ~-11 blue_wool"}],Command:"fill ~ ~7 ~-8 ~ ~6 ~-9 blue_wool"}],Command:"fill ~ ~7 ~-6 ~ ~6 ~-7 blue_wool"}],Command:"fill ~ ~7 ~-3 ~ ~5 ~-5 blue_wool"}],Command:"fill ~ ~7 ~-1 ~ ~7 ~-2 light_blue_wool"}],Command:"fill ~ ~7 ~-1 ~ ~6 ~-1 light_blue_wool"}],Command:"fill ~ ~8 ~-13 ~ ~8 ~-14 light_blue_wool"}],Command:"fill ~ ~9 ~-15 ~ ~8 ~-16 light_blue_wool"}],Command:"setblock ~ ~10 ~-15 cyan_wool"}],Command:"fill ~ ~10 ~-13 ~ ~9 ~-14 light_blue_wool"}],Command:"fill ~ ~10 ~-10 ~ ~8 ~-12 light_blue_wool"}],Command:"fill ~ ~10 ~-7 ~ ~8 ~-9 light_blue_wool"}],Command:"fill ~ ~10 ~-4 ~ ~8 ~-6 light_blue_wool"}],Command:"fill ~ ~10 ~-1 ~ ~8 ~-3 light_blue_wool"}],Command:"fill ~ ~12 ~-16 ~ ~10 ~-16 cyan_wool"}],Command:"fill ~ ~12 ~-14 ~ ~11 ~-15 cyan_wool"}],Command:"fill ~ ~12 ~-12 ~ ~11 ~-13 cyan_wool"}],Command:"fill ~ ~12 ~-1 ~ ~11 ~-1 cyan_wool"}],Command:"fill ~ ~13 ~-13 ~ ~13 ~-16 red_wool"}],Command:"setblock ~ ~13 ~-12 green_wool"}],Command:"fill ~ ~13 ~-11 ~ ~11 ~-11 cyan_wool"}],Command:"fill ~ ~13 ~-8 ~ ~11 ~-10 cyan_wool"}],Command:"fill ~ ~13 ~-5 ~ ~11 ~-7 cyan_wool"}],Command:"fill ~ ~13 ~-2 ~ ~11 ~-4 cyan_wool"}],Command:"setblock ~ ~13 ~-1 green_wool"}],Command:"fill ~ ~14 ~-1 ~ ~14 ~-16 red_wool"}]}'
+
+wool_types = [
+	"white_wool",
+	"orange_wool",
+	"magenta_wool",
+	"light_blue_wool",
+	"yellow_wool",
+	"lime_wool",
+	"pink_wool",
+	"gray_wool",
+	"light_gray_wool",
+	"cyan_wool",
+	"purple_wool",
+	"blue_wool",
+	"brown_wool",
+	"green_wool",
+	"red_wool",
+	"black_wool"
+]
+shulker_types = [
+	"white_shulker_box",
+	"orange_shulker_box",
+	"magenta_shulker_box",
+	"light_blue_shulker_box",
+	"yellow_shulker_box",
+	"lime_shulker_box",
+	"pink_shulker_box",
+	"gray_shulker_box",
+	"light_gray_shulker_box",
+	"cyan_shulker_box",
+	"purple_shulker_box",
+	"blue_shulker_box",
+	"brown_shulker_box",
+	"green_shulker_box",
+	"red_shulker_box",
+	"black_shulker_box"
+]
+
+for i in range(len(wool_types)):
+    command = command.replace(wool_types[i], shulker_types[i])
+#create a txt file with the command
+with open(f'{command_name}.txt', 'w') as f:
+    f.write(command)
